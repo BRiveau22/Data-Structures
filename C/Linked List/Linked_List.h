4d6c195556be9001c17a647b35318d363aaba287
BRiveau <briveau@uri.edu>
2023-04-24 09:39:52 -0400

    Implemented Linked List in C

---
+++ C/Linked List/Linked_List.h
@@ -0,0 +1,16 @@
+//Linked_List.h
+#include "Node.h"
+
+typedef struct LinkedList {
+    Node* head;
+    int size;
+} LinkedList;
+
+LinkedList* create();
+void destroy(LinkedList* list);
+void append(LinkedList* list, int data);
+void prepend(LinkedList* list, int data);
+void insert(LinkedList* list, int data, int index);
+void remove(LinkedList* list, int data);
+bool contains(LinkedList* list, int data);
+void print(LinkedList* list);
