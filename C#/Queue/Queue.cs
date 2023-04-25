e951be272b5a899f9cabd5b6aaa1277eda203b8c
BRiveau <briveau@uri.edu>
2023-04-25 06:59:40 -0400

    Implemented Queue in C#.

---
+++ C#/Queue/Queue.cs
@@ -0,0 +1,81 @@
+public class Node{
+    private int data;
+    private Node next;
+
+    // Constructors
+    public Node() {
+        this.data = 0;
+        this-ndext = null;
+    }
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node (int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+public class Queue{
+    private Node front;
+    private Node back;
+    private int size;
+
+    // Constructors
+    public Queue(){
+        this.front = null;
+        this.back = null;
+        this.size = 0;
+    }
+
+    public Queue(int data){
+        this.front = Node(data);
+        this.back = this.front;
+        this.size = 1;
+    }
+
+    public Queue(int[] arr) {
+        this.front = Node(arr[0]);
+        this.back = this.front;
+        this.size = 1;
+        for (int i = 1; i < arr.Length; i++){
+            this.back.next = Node(arr[i]);
+            this.back = this.back.next;
+            this.size++;
+        }
+    }
+
+    public void enqueue(int date) {
+        if(this.size == 0){
+            this.front = Node(data);
+            this.back = this.front;
+            this.size = 1;
+            return;
+        }
+
+        this.back.next = Node(data);
+        this.back = this.back.next;
+        this.size++;
+    }
+
+    public void dequeue(){
+        if (this.size == 0) {
+            print("Queue is empty");
+            return;
+        }
+        
+        this.front = this.front.next;
+        this.size--;
+    }
+
+    public int peek(){
+        return this.front.data;
+    }
+
+    public int get_size(){
+        return this.size;
+    }
+}
