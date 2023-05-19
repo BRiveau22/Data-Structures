d59ee1d13511f39d09b283033879409b8f280ff1
BRiveau <briveau@uri.edu>
2023-05-19 10:04:42 -0400

    Implemented Hash_Table in C++.

---
+++ C++/Hash Table/Hash_Table.h
@@ -0,0 +1,32 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+#include <string>
+#include <vector>
+
+class Hash_Table{
+private:
+    std::vector<int> _table;
+    int _mode;
+    int _num_collisions;
+    int _num_elements;
+    int _table_size;
+    double _a_value;
+
+    // Methods
+    int _num_digits();
+    int _div_hash(int key);
+    int _mid_square_hash(int key);
+    int _digit_folding_hash(int key);
+    int _mult_hash(int key, double a);
+    void _resize_table();
+public:
+    // Constructors
+    Hash_Table(int mode);
+    Hash_Table(int mode, int table_size);
+    ~Hash_Table();
+
+    // Methods
+    void insert(int key);
+};
+
+#endif
